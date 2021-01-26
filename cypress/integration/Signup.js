994430afe8b4202bf796e1d17418183b99d60556
Carolina Lagerberg <carolina.lagerberg@live.se>
2021-01-26 13:31:33 +0100

    Initial commit

---
+++ cypress/integration/Signup.js
@@ -0,0 +1,24 @@
+describe("Signup form", () => {
+
+    it("Email validation", () => {
+
+        cy.visit("/");
+        cy.get("a").click();
+
+        cy.contains("Registrera!");
+
+        cy.get("form");
+        cy.get('input[name"username"]').type("CoolUser1337");
+        cy.get('input[name="email"]').type("email@");
+        cy.get("form").submit();
+        cy.contains("Felformatterad e-postadress!");
+
+        cy.get("form");
+        cy.get('input[name"username"]').type("CoolUser1337");
+        cy.get('input[name="email"]').type("email@domain.");
+        cy.get("form").submit();
+        cy.contains("Felformatterad e-postadress!").end();
+
+    })
+
+})
